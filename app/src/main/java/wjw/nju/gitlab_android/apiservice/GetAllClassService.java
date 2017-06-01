a32a74829de89fa856ca0a1b1a1cd6a6a597d2cb
WJerry0227 <wangjiawei0227@163.com>
2017-06-01 19:15:07 +0800

    Add Code

---
+++ app/src/main/java/wjw/nju/gitlab_android/apiservice/GetAllClassService.java
@@ -0,0 +1,14 @@
+package wjw.nju.gitlab_android.apiservice;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by wangjiawei on 2017/6/1.
+ */
+
+public class GetAllClassService extends AsyncTask<String,String,String> {
+    @Override
+    protected String doInBackground(String... params) {
+        return null;
+    }
+}
